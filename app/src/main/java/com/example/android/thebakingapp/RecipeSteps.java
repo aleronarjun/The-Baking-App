c0668b858c08a81b51696f1e867ec8b2dd43562c
Arjun Vidyarthi <aleronarjun@gmail.com>
2018-03-13 23:33:03 +0530

    stepslist

--- app/src/main/java/com/example/android/thebakingapp/RecipeSteps.java
+++ app/src/main/java/com/example/android/thebakingapp/RecipeSteps.java
@@ -5,4 +5,37 @@ package com.example.android.thebakingapp;
  */
 
 public class RecipeSteps {
+    private int id;
+    private String shortDesc;
+    private String description;
+    private String videoURL;
+    private String thumbURL;
+
+    public RecipeSteps(int id, String shortDesc, String description, String videoURL, String thumbURL){
+        this.id = id;
+        this.shortDesc = shortDesc;
+        this.description = description;
+        this.videoURL = videoURL;
+        this.thumbURL = thumbURL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public String getThumbURL() {
+        return thumbURL;
+    }
 }
--- app/src/main/java/com/example/android/thebakingapp/StepAdapter.java
+++ app/src/main/java/com/example/android/thebakingapp/StepAdapter.java
@@ -6,8 +6,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Arjun Vidyarthi on 11-Mar-18.
@@ -50,17 +54,34 @@ public class StepAdapter extends RecyclerView.Adapter<StepAdapter.StepAdapterVie
             return;
         }
 
+//        int currentStepid = recipeSteps.get(position).getId();
+        String currentStepShortDesc = recipeSteps.get(position).getShortDesc();
+//        String currentStepDesc = recipeSteps.get(position).getDescription();
+//        String currentStepVideo = recipeSteps.get(position).getVideoURL();
+//        String currentStepThumb = recipeSteps.get(position).getThumbURL();
+
+        holder.stepShortDesc.setText(currentStepShortDesc);
 
     }
 
     @Override
     public int getItemCount() {
+        if(recipeSteps==null)
         return 0;
+
+        return recipeSteps.size();
     }
 
     public class StepAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.step_short)
+        TextView stepShortDesc;
+
         public StepAdapterViewHolder(View itemView) {
             super(itemView);
+
+            ButterKnife.bind(this, itemView);
+
+            itemView.setOnClickListener(this);
         }
 
         @Override
